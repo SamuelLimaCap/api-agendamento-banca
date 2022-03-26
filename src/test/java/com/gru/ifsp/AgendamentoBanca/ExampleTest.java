550db8e5b87cc104bbd45e614a385badc9430cf3
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-03-25 22:57:58 -0300

    First bleeding commit

---
+++ src/test/java/com/gru/ifsp/AgendamentoBanca/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.gru.ifsp.AgendamentoBanca;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void return4() {
+
+        Assert.assertEquals("teste1", "admin@admin.com", "oi");
+    }
+}
