877e51b8ef2c2c3279f1813806c1b1c136575a82
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-03-25 22:39:57 -0300

    Second commit

---
+++ src/main/java/com/gru/ifsp/AgendamentoBanca/AgendamentoBancaApplication.java
@@ -0,0 +1,13 @@
+package com.gru.ifsp.AgendamentoBanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoBancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoBancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gru/ifsp/AgendamentoBanca/AgendamentoBancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gru.ifsp.AgendamentoBanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoBancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
