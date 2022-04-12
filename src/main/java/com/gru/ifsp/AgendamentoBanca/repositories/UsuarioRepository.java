80952456890877abbfb75f4bb4bf5e1d16579b74
CaioPaiva1911 <caiowick@hotmail.com>
2022-04-12 08:36:22 -0300

    updates of branch local

--- src/main/java/com/gru/ifsp/AgendamentoBanca/repositories/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gru.ifsp.AgendamentoBanca.repositories;
-
-import com.gru.ifsp.AgendamentoBanca.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
